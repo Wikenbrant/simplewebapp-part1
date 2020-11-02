8dba1afd9abc92f8af4f85d8ff66152768cff657
MaxBane807 <55383373+MaxBane807@users.noreply.github.com>
2020-11-02 09:22:03 +0100

    Added model classes

---
+++ SimpleWebAppMVC/Models/Epic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebAppMVC.Models
+{
+    public class Epic
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<Story> Stories { get; set; }
+    }
+}
